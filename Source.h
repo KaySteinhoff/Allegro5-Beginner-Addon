a14351101264cb471f6064b6f3192632e4ad99b7
Kay STeinhoff <kay.steinhoff@web.de>
2021-07-15 13:33:17 +0200

    Add files via upload

---
+++ Source.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Addon.h"
+#include "Matrix.h"
+using namespace std;
+
+//<summary>
+//This class is used to programm the behavior of the Window.
+//It is simplified so that a Person with little knowledge can
+//addapt easily to this Addon.
+//Obviously this is very much inspired by the workflow in Unity
+//</summary>
+class Source
+{
+	//Declaration of the variables (can be added in the Function too like in Unity)
+	Matrix mat;
+	Mesh mesh;
+
+public:
+	//Called at the start of the Application
+	void Start()
+	{
+		mat.setup_3D();
+		mesh.LoadFromObjectFile("icosphere.obj");
+	}
+
+	//Called everyframe
+	void Update()
+	{
+		mat.UpdateMatrix(0.03 , mesh);
+	}
+};
