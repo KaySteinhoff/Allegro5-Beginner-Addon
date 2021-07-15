453449803dc8739a7e3b9ca20cd38bc03e9ab2a6
Kay STeinhoff <kay.steinhoff@web.de>
2021-07-15 13:39:19 +0200

    Add files via upload

---
+++ Kast v_1.1.1(Experimental)/Matrix.h
@@ -0,0 +1,278 @@
+#pragma once
+#include <fstream>
+#include <strstream>
+#include <algorithm>
+#include <vector>
+#include "allegro5/allegro.h"
+#include "allegro5/allegro_image.h"
+#include "allegro5/keyboard.h"
+#include "allegro5/keycodes.h"
+#include "allegro5/allegro_native_dialog.h"
+#include "allegro5/allegro_ttf.h"
+#include "allegro5/allegro_font.h"
+#include "allegro5/allegro_primitives.h"
+#include "allegro5/allegro_image.h"
+using namespace std;
+
+/// <summary>
+/// This Matrix was writen by javidx9 in his series 'Code-It-Yourself!' Part2
+/// It supports .obj files and is in its experimental state. Should any bug be found
+/// and/or the finder has writen a solution to that please send it to this email 
+/// I would be very greatful: kay.steinhoff@web.de
+/// 
+/// Have fun and enjoy seeing your 3d models in my shitty Backend system :D
+/// </summary>
+
+ALLEGRO_DISPLAY* display;
+
+struct vec3d
+{
+	float x, y, z;
+};
+
+struct triangle
+{
+	vec3d p[3];
+
+	ALLEGRO_COLOR col;
+};
+
+struct Mesh
+{
+	vector<triangle> tris;
+
+	bool LoadFromObjectFile(string sFilename)
+	{
+		ifstream f(sFilename);
+		if (!f.is_open())
+			return false;
+
+		// Local cache of verts
+		vector<vec3d> verts;
+
+		while (!f.eof())
+		{
+			char line[128];
+			f.getline(line, 128);
+
+			strstream s;
+			s << line;
+
+			char junk;
+
+			if (line[0] == 'v')
+			{
+				vec3d v;
+				s >> junk >> v.x >> v.y >> v.z;
+				verts.push_back(v);
+			}
+
+			if (line[0] == 'f')
+			{
+				int f[3];
+				s >> junk >> f[0] >> f[1] >> f[2];
+				tris.push_back({ verts[f[0] - 1], verts[f[1] - 1], verts[f[2] - 1] });
+			}
+		}
+
+		return true;
+	}
+
+};
+
+struct mat4x4
+{
+	float m[4][4] = { 0 };
+};
+
+void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m)
+{
+	o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+	o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+	o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+	float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+	if (w != 0.0f)
+	{
+		o.x /= w; o.y /= w; o.z /= w;
+	}
+}
+
+struct Matrix
+{
+public:
+
+	vec3d vCamera;
+	mat4x4 matProj;
+
+	float fTheta = 0;	
+
+	void setup_3D()
+	{
+		float fNear = 0.1f;
+		float fFar = 1000.0f;
+		float fFov = 90.0f;
+		float fAspectRatio = (float)al_get_display_height(display) / (float)al_get_display_width(display);
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+
+		matProj.m[0][0] = fAspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar / (fFar - fNear);
+		matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+		matProj.m[2][3] = 1.0f;
+		matProj.m[3][3] = .0f;
+	}
+
+	ALLEGRO_COLOR _GetColor(float lum)
+	{
+		ALLEGRO_COLOR fg_col;
+		int pixel_bw = (int)(13.0f * lum);
+		switch (pixel_bw)
+		{
+			case 0: fg_col = al_map_rgb(0, 0, 0); break;
+
+			case 1: fg_col = al_map_rgb(25, 25, 25); break;
+			case 2: fg_col = al_map_rgb(45, 45, 45); break;
+			case 3: fg_col = al_map_rgb(65, 65, 65); break;
+			case 4: fg_col = al_map_rgb(85, 85, 85); break;
+					
+			case 5: fg_col = al_map_rgb(100, 100, 100); break;
+			case 6: fg_col = al_map_rgb(120, 120, 120); break;
+			case 7: fg_col = al_map_rgb(145, 145, 145); break;
+			case 8: fg_col = al_map_rgb(165, 165, 165); break;
+					
+			case 9: fg_col = al_map_rgb(180, 180, 180); break;
+			case 10: fg_col = al_map_rgb(200, 200, 200); break;
+			case 11: fg_col = al_map_rgb(220, 220, 220); break;
+			case 12: fg_col = al_map_rgb(255, 255, 255); break;
+			default:
+				fg_col = al_map_rgb(0, 0, 0);
+		}
+
+		ALLEGRO_COLOR c;
+		c = fg_col;
+		return c;
+	}
+
+	void UpdateMatrix(float fElapsedTime, Mesh mesh)
+	{
+		// Set up rotation matrices
+		mat4x4 matRotZ, matRotX;
+		fTheta += 1.0f * fElapsedTime;
+
+		// Rotation Z
+		matRotZ.m[0][0] = cosf(fTheta);
+		matRotZ.m[0][1] = sinf(fTheta);
+		matRotZ.m[1][0] = -sinf(fTheta);
+		matRotZ.m[1][1] = cosf(fTheta);
+		matRotZ.m[2][2] = 1;
+		matRotZ.m[3][3] = 1;
+
+		// Rotation X
+		matRotX.m[0][0] = 1;
+		matRotX.m[1][1] = cosf(fTheta * 0.5f);
+		matRotX.m[1][2] = sinf(fTheta * 0.5f);
+		matRotX.m[2][1] = -sinf(fTheta * 0.5f);
+		matRotX.m[2][2] = cosf(fTheta * 0.5f);
+		matRotX.m[3][3] = 1;
+
+		// Store triagles for rastering later
+		vector<triangle> vecTrianglesToRaster;
+
+		// Draw Triangles
+		for (auto tri : mesh.tris)
+		{
+			triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
+
+			// Rotate in Z-Axis
+			MultiplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
+			MultiplyMatrixVector(tri.p[1], triRotatedZ.p[1], matRotZ);
+			MultiplyMatrixVector(tri.p[2], triRotatedZ.p[2], matRotZ);
+
+			// Rotate in X-Axis
+			MultiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
+
+			// Offset into the screen
+			triTranslated = triRotatedZX;
+			triTranslated.p[0].z = triRotatedZX.p[0].z + 8.0f;
+			triTranslated.p[1].z = triRotatedZX.p[1].z + 8.0f;
+			triTranslated.p[2].z = triRotatedZX.p[2].z + 8.0f;
+
+			// Use Cross-Product to get surface normal
+			vec3d normal, line1, line2;
+			line1.x = triTranslated.p[1].x - triTranslated.p[0].x;
+			line1.y = triTranslated.p[1].y - triTranslated.p[0].y;
+			line1.z = triTranslated.p[1].z - triTranslated.p[0].z;
+
+			line2.x = triTranslated.p[2].x - triTranslated.p[0].x;
+			line2.y = triTranslated.p[2].y - triTranslated.p[0].y;
+			line2.z = triTranslated.p[2].z - triTranslated.p[0].z;
+
+			normal.x = line1.y * line2.z - line1.z * line2.y;
+			normal.y = line1.z * line2.x - line1.x * line2.z;
+			normal.z = line1.x * line2.y - line1.y * line2.x;
+
+			// It's normally normal to normalise the normal
+			float l = sqrtf(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
+			normal.x /= l; normal.y /= l; normal.z /= l;
+
+			//if (normal.z < 0)
+			if (normal.x * (triTranslated.p[0].x - vCamera.x) +
+				normal.y * (triTranslated.p[0].y - vCamera.y) +
+				normal.z * (triTranslated.p[0].z - vCamera.z) < 0.0f)
+			{
+				// Illumination
+				vec3d light_direction = { 0.0f, 0.0f, -1.0f };
+				float l = sqrtf(light_direction.x * light_direction.x + light_direction.y * light_direction.y + light_direction.z * light_direction.z);
+				light_direction.x /= l; light_direction.y /= l; light_direction.z /= l;
+
+				// How similar is normal to light direction
+				float dp = normal.x * light_direction.x + normal.y * light_direction.y + normal.z * light_direction.z;
+
+				// Choose console colours as required (much easier with RGB)
+				triTranslated.col = _GetColor(dp);
+
+				// Project triangles from 3D --> 2D
+				MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+				MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+				MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+				triProjected.col = triTranslated.col;
+
+				// Scale into view
+				triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
+				triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
+				triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
+				triProjected.p[0].x *= 0.5f * (float)al_get_display_width(display);
+				triProjected.p[0].y *= 0.5f * (float)al_get_display_height(display);
+				triProjected.p[1].x *= 0.5f * (float)al_get_display_width(display);
+				triProjected.p[1].y *= 0.5f * (float)al_get_display_height(display);
+				triProjected.p[2].x *= 0.5f * (float)al_get_display_width(display);
+				triProjected.p[2].y *= 0.5f * (float)al_get_display_height(display);
+
+				// Store triangle for sorting
+				vecTrianglesToRaster.push_back(triProjected);
+			}
+
+		}
+
+		// Sort triangles from back to front
+		sort(vecTrianglesToRaster.begin(), vecTrianglesToRaster.end(), [](triangle& t1, triangle& t2)
+			{
+				float z1 = (t1.p[0].z + t1.p[1].z + t1.p[2].z) / 3.0f;
+				float z2 = (t2.p[0].z + t2.p[1].z + t2.p[2].z) / 3.0f;
+				return z1 > z2;
+			});
+
+		for (auto& triProjected : vecTrianglesToRaster)
+		{
+			// Rasterize triangle
+			al_draw_filled_triangle(triProjected.p[0].x, triProjected.p[0].y,
+				triProjected.p[1].x, triProjected.p[1].y,
+				triProjected.p[2].x, triProjected.p[2].y,
+				triProjected.col);
+		}
+	}
+
+};
